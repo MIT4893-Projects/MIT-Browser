65d5c6c9c45f7b0f8f532489555adafca4abb0bc
MIT4893-Projects <minhngoc2891211@gmail.com>
2022-11-27 19:12:56 +0700

    add URL bar

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import os
 import sys
 from PySide2 import (
     QtCore as QtC,
+    QtGui as QtG,
     QtWidgets as QtW,
     QtWebEngineWidgets as QtWEW
 )
@@ -21,6 +22,43 @@ class Browser(QtW.QMainWindow):
 
     __webview : QtWEW.QWebEngineView
     __frame_main : QtW.QFrame
+
+    class URLBar(QtW.QFrame):
+        """Create an widget stands for URL bar in browser
+
+        Inherit:
+            PySide2.QtWidgets.QFrame
+        """
+
+        __button_back : QtW.QPushButton
+        __button_next : QtW.QPushButton
+        __lineedit_urlbar : QtW.QLineEdit
+
+        def __init__(self):
+            super().__init__()
+
+            self.setMinimumHeight(32)
+
+            self.setLayout(QtW.QHBoxLayout())
+            self.layout().setMargin(1)
+            self.layout().setSpacing(1)
+
+            self.__button_next = QtW.QPushButton()
+            self.__button_back = QtW.QPushButton()
+            self.__button_next.setIcon(QtG.QIcon('./assets/icons/next-arrow.png'))
+            self.__button_back.setIcon(QtG.QIcon('./assets/icons/back-arrow.png'))
+            self.__button_back.setMinimumWidth(30)
+            self.__button_next.setMinimumWidth(30)
+            self.__button_back.setSizePolicy(QtW.QSizePolicy.Minimum, QtW.QSizePolicy.Expanding)
+            self.__button_next.setSizePolicy(QtW.QSizePolicy.Minimum, QtW.QSizePolicy.Expanding)
+            self.layout().addWidget(self.__button_back)
+            self.layout().addWidget(self.__button_next)
+
+            self.__lineedit_urlbar = QtW.QLineEdit()
+            self.__lineedit_urlbar.setSizePolicy(QtW.QSizePolicy.Expanding, QtW.QSizePolicy.Expanding)
+            self.__lineedit_urlbar.setObjectName("urlbar")
+            self.__lineedit_urlbar.setStyleSheet(STYLESHEET)
+            self.layout().addWidget(self.__lineedit_urlbar)
 
     def __init__(self):
         super().__init__()
@@ -47,6 +85,7 @@ class Browser(QtW.QMainWindow):
         self.__frame_main.setLayout(QtW.QGridLayout())
         self.__frame_main.layout().setSpacing(0)
         self.__frame_main.layout().setMargin(0)
+        self.__frame_main.layout().addWidget(self.URLBar())
         self.__frame_main.layout().addWidget(self.__webview)
 
         self.setCentralWidget(self.__frame_main)
@@ -56,8 +95,6 @@ class Browser(QtW.QMainWindow):
         self.__webview.load(QtC.QUrl("https://bing.com"))
 
         self.show()
-        self.showMaximized()
-
 
 
 
@@ -75,6 +112,10 @@ def get_absolute_path(path):
 
 
 if __name__ == "__main__":
+    with open("pyqt5.css", encoding="utf-8") as stylesheet_file:
+        global STYLESHEET
+        STYLESHEET = stylesheet_file.read()
+
     app = QtW.QApplication(sys.argv)
 
     web_view = Browser()
