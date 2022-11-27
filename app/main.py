b8f9ef6e3112836bafdf8f0c648734b69412f361
MIT4893-Projects <minhngoc2891211@gmail.com>
2022-11-27 14:40:22 +0700

    update to load "index.html" with relative directory

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
+"""
+Main Python file for Browser Window
+"""
+
+import sys
 from PySide2 import (
-    QtCore as QtC, 
+    QtCore as QtC,
     QtWidgets as QtW,
     QtWebEngineWidgets as QtWEW,
-    QtWebChannel as QtWC
+    # QtWebChannel as QtWC
 )
 
 
-class Browser(QtWEW.QWebEngineView):
-    ...
 
+class Browser(QtWEW.QWebEngineView):
+    """Browser class for render HTML and browse webs
+
+    Inherit:
+        PySide2.QtWebEngineWidgets.QWebEngineView
+    """
+
+
+
+if __name__ == "__main__":
+    app = QtW.QApplication(sys.argv)
+
+    web_view = QtWEW.QWebEngineView()
+    web_view.load(QtC.QUrl("./index.html"))
+    web_view.show()
+
+    app.exec_()
