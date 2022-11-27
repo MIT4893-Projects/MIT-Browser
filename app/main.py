14fd020a24565959770c402682c8050b085a3a9d
MIT4893-Projects <minhngoc2891211@gmail.com>
2022-11-27 14:22:33 +0700

    add Browser class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-import tkinter
+from PySide2 import (
+    QtCore as QtC, 
+    QtWidgets as QtW,
+    QtWebEngineWidgets as QtWEW,
+    QtWebChannel as QtWC
+)
+
+
+class Browser(QtWEW.QWebEngineView):
+    ...
+
